92deacd7610a7bb80e84d8a2c5e30ce4dd503f9f
zaphod1984 <zaphod84@gmx.de>
2015-06-12 17:27:36 +0200

    add trivial solutions for tsp

---
+++ implementations/tsp/combinations.js
@@ -0,0 +1,17 @@
+function* createCombinations(input) {
+    if (input.length == 1) {
+        yield input;
+        return;
+    }
+
+    var result = [];
+    for(var i = 0; i < input.length; i++) {
+        var tail = input.concat([]);
+        var head = input[i];
+        tail.splice(i, 1);
+
+        for (var combination of createCombinations(tail)) {
+            yield [head].concat(combination);
+        }
+    }
+}
