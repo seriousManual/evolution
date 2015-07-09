10c0c177b86895db18fea6ab2e28ca35b121630b
zaphod1984 <zaphod84@gmx.de>
2015-07-09 12:29:35 +0200

    webworkers up and running

--- src/js/lib/geneticAlgorithm/GeneticAlgorithm.js
+++ src/js/lib/geneticAlgorithm/GeneticAlgorithm.js
@@ -10,8 +10,8 @@ function GeneticAlgorithm(options) {
     Emitter.call(this);
 
     this._population = null;
-    this._maxRunTime = options.maxRunTime || Infinity;
     this._interval = options.interval || 1000;
+    this._intervalHandle = null;
     this._numberRuns = 0;
     this._sampler = new Sampler();
 
@@ -80,36 +80,63 @@ GeneticAlgorithm.prototype.calculateFitness = function () {
 };
 
 GeneticAlgorithm.prototype.run = function () {
-    var that = this;
-    var population = that.getPopulation();
+    if (this._interval == 'fast') {
+        this._runFast();
+    } else {
+        this._runByInterval();
+    }
+};
 
-    var handle = setInterval(function() {
-        that._numberRuns++;
-        that._sampler.sample();
+GeneticAlgorithm.prototype._runByInterval = function() {
+    this._intervalHandle = setInterval(this._step.bind(this));
+};
 
-        var parents = that.chooseParents();
-        var bestIndividuum = population.getFirst();
-
-        var child = parents[0].recombinate(parents[1]);
-        child.mutate();
-        child.setFitness(that.calculateFitness(child));
-
-        that.emit('childCheck', child);
-
-        if (population.fitsIn(child)) {
-            population.replaceLastIndividuum(child);
+GeneticAlgorithm.prototype._runFast = function() {
+    while(true) {
+        if (!this._step()) {
+            return;
         }
+    }
+};
 
-        if (population.getFirst() !== bestIndividuum) {
-            that.emit('newOptimum', population.getFirst());
-        }
+GeneticAlgorithm.prototype._step = function() {
+    var population = this.getPopulation();
 
-        if (that.termCriterium()) {
-            clearInterval(handle);
-            that._sampler.stop();
-            that.emit('terminated', population);
-        }
-    }, this._interval);
+    this._numberRuns++;
+    this._sampler.sample();
+
+    var parents = this.chooseParents();
+    var bestIndividuum = population.getFirst();
+
+    var child = parents[0].recombinate(parents[1]);
+    child.mutate();
+    child.setFitness(this.calculateFitness(child));
+
+    this.emit('childCheck', child);
+
+    if (population.fitsIn(child)) {
+        population.replaceLastIndividuum(child);
+    }
+
+    if (population.getFirst() !== bestIndividuum) {
+        this.emit('newOptimum', population.getFirst());
+    }
+
+    if (this.termCriterium()) {
+        this._terminate();
+        return false;
+    }
+
+    return true;
+};
+
+GeneticAlgorithm.prototype._terminate = function() {
+    if (this._intervalHandle) {
+        clearInterval(this._intervalHandle);
+    }
+
+    this._sampler.stop();
+    this.emit('terminated', this.getPopulation());
 };
 
 module.exports = GeneticAlgorithm;
