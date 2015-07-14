f229b0d9ce98c1ef4746558cd4f6199727c412bb
zaphod1984 <zaphod84@gmx.de>
2015-07-14 09:07:40 +0200

    makeup

--- src/js/scenarios/tsp/tspWebworker.js
+++ src/js/scenarios/tsp/tspWebworker.js
@@ -2,6 +2,7 @@ var worker = require('webworkify');
 
 var City = require('../../lib/tsp/lib/City');
 var Printer = require('../../lib/tsp/lib/Printer');
+var tspWebworker = require('../../lib/tsp/TspWebworker');
 
 function Tsp(canvasId, options) {
     this._printer = new Printer(canvasId, options);
@@ -20,7 +21,7 @@ Tsp.prototype.addCity = function (x, y) {
 };
 
 Tsp.prototype.run = function () {
-    var algorithm = worker(require('../../lib/tsp/TspWebworker'));
+    var algorithm = worker(tspWebworker);
     var course = this._printer.createCourse({lineWidth: 2, lineColor: 'rgb(255, 0, 0)'});
 
     algorithm.addEventListener('message', function (message) {
