6cd994be64fc5a7663762321322fc2b0553d112e
zaphod1984 <zaphod84@gmx.de>
2015-08-11 09:31:07 +0200

    frontend

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,26 @@ var express = require('express');
 var gulp = require('gulp');
 var bower = require('gulp-bower');
 var concat = require('gulp-concat');
+var less = require('gulp-less');
 var rimraf = require('rimraf');
 var browserify = require('gulp-browserify');
 var sequence = require('run-sequence');
 var gutil = require('gulp-util');
 
 var SOURCE_DIR = 'src';
-var BUILD_TARGET_DIR = 'frontend/script';
+var BUILD_TARGET_DIR_SCRIPT = 'frontend/script';
+var BUILD_TARGET_DIR_STYLE = 'frontend/style';
 var REVEAL_TARGET_DIR = 'frontend/slides/reveal';
 
 gulp.task('clean', function (callback) {
-    rimraf(BUILD_TARGET_DIR, function (error) {
+    rimraf(BUILD_TARGET_DIR_SCRIPT, function (error) {
         if (error) return callback(error);
 
-        rimraf(REVEAL_TARGET_DIR, callback);
+        rimraf(BUILD_TARGET_DIR_STYLE, function(error) {
+            if (error) return callback(error);
+
+            rimraf(REVEAL_TARGET_DIR, callback);
+        });
     });
 });
 
@@ -25,7 +31,7 @@ gulp.task('scripts', function () {
     return gulp
         .src(SOURCE_DIR + '/js/app.js')
         .pipe(browserify()).on('error', gutil.log)
-        .pipe(gulp.dest(BUILD_TARGET_DIR));
+        .pipe(gulp.dest(BUILD_TARGET_DIR_SCRIPT));
 });
 
 gulp.task('bower_components', function () {
@@ -38,7 +44,23 @@ gulp.task('libscripts', ['bower_components'], function () {
         'bower_components/fabric/dist/fabric.min.js'
     ])
         .pipe(concat('vendor.js'))
-        .pipe(gulp.dest(BUILD_TARGET_DIR));
+        .pipe(gulp.dest(BUILD_TARGET_DIR_SCRIPT));
+});
+
+////////////////////////////////////////////////////// style //////////////////////////////////////////////////
+
+gulp.task('style', ['bower_components'], function () {
+    return gulp
+        .src(['bower_components/bootstrap/dist/css/bootstrap.css'])
+        .pipe(concat('external.css'))
+        .pipe(gulp.dest(BUILD_TARGET_DIR_STYLE));
+});
+
+gulp.task('style_app', function () {
+    return gulp
+        .src(SOURCE_DIR + '/style/style.less')
+        .pipe(less()).on('error', gutil.log)
+        .pipe(gulp.dest(BUILD_TARGET_DIR_STYLE));
 });
 
 ////////////////////////////////////////////////////// slides //////////////////////////////////////////////////
@@ -56,7 +78,7 @@ gulp.task('staticserve', function (callback) {
         .listen(port, function(error) {
             if (error) throw new Error(error);
 
-            console.log('static server running, visit http://localhost:' + port + '/slides/slides.html');
+            console.log('static server running, visit http://localhost:' + port);
 
             callback();
         });
@@ -67,6 +89,7 @@ gulp.task('staticserve', function (callback) {
 gulp.task('dev', function (callback) {
     build(function () {
         gulp.watch(SOURCE_DIR + '/js/**/*.js', ['scripts']);
+        gulp.watch(SOURCE_DIR + '/style/**/*.less', ['style_app']);
 
         callback();
     });
@@ -76,10 +99,10 @@ gulp.task('build', function (callback) {
     build(callback);
 });
 
-gulp.task('slides', function (callback) {
+gulp.task('serve', function (callback) {
     sequence('build', 'staticserve', callback);
 });
 
 function build (callback) {
-    sequence('clean', ['libscripts', 'scripts', 'reveal'], callback);
+    sequence('clean', ['style', 'style_app', 'libscripts', 'scripts', 'reveal'], callback);
 }
