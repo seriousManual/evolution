d688c1b7c4577a0d8de15b3cd919cf11de8cec7e
zaphod1984 <zaphod84@gmx.de>
2015-08-11 08:55:18 +0200

    restructure tsp

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 window.EVO = {
-    tspCombinations: require('./scenarios/tsp/combinations'),
-    tspRecombination: require('./scenarios/tsp/recombination'),
-    tspPrinter: require('./scenarios/tsp/printer'),
-    tspTrivial: require('./scenarios/tsp/trivial'),
-    tsp: require('./scenarios/tsp/tsp'),
-    tspWebworker: require('./scenarios/tsp/tspWebworker'),
+    tsp: {
+        tspCombinations: require('./scenarios/tsp/combinations'),
+        tspRecombination: require('./scenarios/tsp/recombination'),
+        tspPrinter: require('./scenarios/tsp/printer'),
+        tspTrivial: require('./scenarios/tsp/trivial'),
+        tsp: require('./scenarios/tsp/tsp'),
+        tspWebworker: require('./scenarios/tsp/tspWebworker')
+    },
 
     circles: {
         printer: require('./scenarios/circles/printer'),
