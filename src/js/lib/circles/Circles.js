844be3d89bde6d84a3ef6cf47c6d2e5209409793
Manuel Ernst <manuel.ernst@methodpark.de>
2019-02-22 11:27:16 +0100

    add mutation rate example

--- src/js/lib/circles/Circles.js
+++ src/js/lib/circles/Circles.js
@@ -22,8 +22,10 @@ Circles.prototype._createPopulation = function () {
     var population = new Population();
 
     for (var i = 0; i < this._sizePopulation; i ++) {
+        var radius = parseInt(this._options.width * 0.1, 10);
+        console.log(radius)
         population.addIndividuum(new Circle()
-            .setRadius(parseInt(Math.random() * this._options.width * 0.4, 10))
+            .setRadius(radius)
             .setX(parseInt(Math.random() * this._options.width, 10))
             .setY(parseInt(Math.random() * this._options.height, 10))
         );
@@ -37,12 +39,15 @@ Circles.prototype.calculateFitness = function (child) {
     var overlapping = 0;
     var outside = 0;
 
+
+    //overlapping with another circle
     this._scenario.forEach(function (scenarioCircle) {
-        if (that._pyth(scenarioCircle, child) < child.getRadius() + scenarioCircle.getRadius()) {
+        if (that._distance(scenarioCircle, child) < (child.getRadius() + scenarioCircle.getRadius())) {
             overlapping++;
         }
     });
 
+    //overlapping with the playground border
     if (child.getX() - child.getRadius() < 0) {
         outside++;
     }
@@ -56,34 +61,51 @@ Circles.prototype.calculateFitness = function (child) {
         outside++;
     }
 
+    //positioned outside of playground
     if (child.getY() < 0 || child.getX() < 0 || child.getX() > this._options.width || child.getY() > this._options.height) {
         outside += 10;
     }
 
     var area = child.area();
     if (area <= 0) {
-        outside += 100;
+        return -Infinity;
     }
 
-    var rating = 1;
-    if (outside > 0 || overlapping > 0) {
-        rating = -1;
-
-        if (outside > 0) {
-            rating = rating * outside * 4;
-        }
-
-        if (overlapping > 0) {
-            rating = rating * overlapping * 2;
-        }
+    if (outside) {
+        return -1 * area * 10000;
     }
 
-    rating = rating * area;
+    if (overlapping) {
+        return -1 * area * 100;
+    }
 
-    return rating;
+    return area;
+
+    // var area = child.area();
+    // if (area <= 0) {
+    //     outside += 100;
+    // }
+
+
+    // var rating = 1;
+    // if (outside > 0 || overlapping > 0) {
+    //     rating = -1;
+
+    //     if (outside > 0) {
+    //         rating = rating * outside * 4;
+    //     }
+
+    //     if (overlapping > 0) {
+    //         rating = rating * overlapping * 2;
+    //     }
+    // }
+
+    // rating = rating * area;
+
+    // return rating;
 };
 
-Circles.prototype._pyth = function(c1, c2) {
+Circles.prototype._distance = function(c1, c2) {
     return Math.sqrt(Math.pow(c1.getX() - c2.getX(), 2) + Math.pow(c1.getY() - c2.getY(), 2));
 };
 
